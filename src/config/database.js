d399fbec1389f9c9552de0b26f63fc0a84f62156
Félix Vicente <felixvicent1306@gmail.com>
2020-04-06 13:22:16 -0300

    sequelize

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'felps',
+    password: 'password',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
